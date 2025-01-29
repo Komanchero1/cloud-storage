03def418304a8c0dccdc4cf0287ec9bb044202ec
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2025-01-29 12:40:43 +0400

    Delete src directory

--- src/main/java/org/example/cloudstorage/exeption/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.cloudstorage.exeption;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);//если пользователь не найден отправляется сообщение об ошибке
-    }
-}
