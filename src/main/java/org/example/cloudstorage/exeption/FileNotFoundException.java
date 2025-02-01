dddecbe5006428d471add8f0a96752433a5fd8f4
Dmitriy <160003751+Komanchero1@users.noreply.github.com>
2025-02-01 20:21:38 +0400

    Delete src directory

--- src/main/java/org/example/cloudstorage/exeption/FileNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.example.cloudstorage.exeption;
-
-public class FileNotFoundException extends RuntimeException {
-
-    public FileNotFoundException(String message) {
-        super(message);//сообщение, что файл не был найден
-    }
-}
--- src/main/java/org/example/cloudstorage/exeption/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package org.example.cloudstorage.exeption;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);//если неправильный логин или пароль отправляется сообщение об ошибке
-    }
-}
